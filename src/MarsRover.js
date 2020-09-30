7be8d0798761df4a99eb4f9f615637313a0c1640
ogwenya <emanuelogwenya@gmail.com>
2020-09-30 17:31:53 +0300

    increased number of photos

--- src/MarsRover.js
+++ src/MarsRover.js
@@ -10,7 +10,7 @@ class MarsRover extends Component {
         axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=VHyEHQdHVsM7DSm446QvcVInCbGphBJnYCbveK9S')
             .then((res) => {
                     this.setState({
-                        pictures: res.data.photos.slice(0, 10)
+                        pictures: res.data.photos.slice(0, 200)
                     })
             })
             .then(res => console.log(this.state.pictures))
